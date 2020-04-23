18c99a6b6cd6478c1f85223c538afd2277fd97af
Terra <terra.taylor@gmail.com>
2020-04-23 13:48:02 -0600

    react game build

---
+++ build/precache-manifest.4c2e97c6752c4461d785585da49603f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313c15b90d8a79de7d90cd99ccb25b0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e00c241fa901b5f3e9b",
+    "url": "/static/css/main.3655b477.chunk.css"
+  },
+  {
+    "revision": "c82c226095432ff82bfe",
+    "url": "/static/js/2.0b2c73a4.chunk.js"
+  },
+  {
+    "revision": "8e00c241fa901b5f3e9b",
+    "url": "/static/js/main.9b6b91c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
