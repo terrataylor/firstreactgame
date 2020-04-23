1a8fee42bd885718c2f45dae953a04c61e8b96c8
Terra <terra.taylor@gmail.com>
2020-04-23 14:00:44 -0600

    Updates

---
+++ precache-manifest.856519a873a46bca3a993ac052381f50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f64cc984e7761a7e828b3be0233e972",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "b0fd4a1b1c33e1075082",
+    "url": "/myapp/static/css/main.f2add518.chunk.css"
+  },
+  {
+    "revision": "2eddc907f4110be8f8a4",
+    "url": "/myapp/static/js/2.cbd58dce.chunk.js"
+  },
+  {
+    "revision": "b0fd4a1b1c33e1075082",
+    "url": "/myapp/static/js/main.774e61a8.chunk.js"
+  },
+  {
+    "revision": "f56cbfd09050d46037d5",
+    "url": "/myapp/static/js/runtime~main.e4f53ea3.js"
+  }
+]);
